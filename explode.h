7eebb563540d1cb69e4a8219a05ca83db7144254
David Blum <shezukesanoora@gmail.com>
2019-01-29 01:04:15 +0200

    Fixes

--- explode.h
+++
@@ -1,8 +0,0 @@
-#ifndef FAST_SPLIT_EXPLODE_H
-#define FAST_SPLIT_EXPLODE_H
-
-#include "exploder.h"
-
-exploder explode(const char *source, const char *delim, const size_t limit = 0);
-
-#endif //FAST_SPLIT_EXPLODE_H
